e2d5b7da70c3b11bfb76c9ab28ed63c09e11487c
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-17 19:05:57 +0300

    all test pass

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
     # code goes here!
     pass
 
 def square_integers(int_list):
+    squared_numbers = []
+    for number in int_list:
+        squared_numbers.append(number**2)
+    return squared_numbers
     # code goes here!
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
